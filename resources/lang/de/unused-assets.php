3066555999ef471c9ca4c1ca23548360f01089c9
Kyle Werner <kyle.werner@novu.ch>
2023-06-23 10:11:52 +0200

    fix typo in translation

--- resources/lang/de/unused-assets.php
+++ resources/lang/de/unused-assets.php
@@ -4,6 +4,6 @@ return [
     'title' => 'Nicht verwendete Dateien',
     'edit' => 'Diese Datei bearbeiten',
     'explanation' => 'Dateien können in den Sammlungen, Taxonomien und Globalen Inhalten verwendet werden. Wenn eine Datei nicht verwendet wird, wird sie in dieser Liste angezeigt.',
-    'count' => '{0}|{1} :amount Datei ist ungenutzt.|[2,*] :amount Dateinen sind ungenutzt',
+    'count' => '{0}|{1} :amount Datei ist ungenutzt.|[2,*] :amount Dateien sind ungenutzt',
     'done' => 'Es gibt keine ungenutzten Dateien.',
 ];
